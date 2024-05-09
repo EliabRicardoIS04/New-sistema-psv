01affcf7c2b751dd8c25e85f4ff7d43504f11b9d
LuisRicardo77 <luissayas2018@gmail.com>
2024-05-09 11:38:19 -0500

    "creacion y comproblacion de la interface ITurnoRepository"

---
+++ Models/Contracts/ITurnoRepository.php
@@ -0,0 +1,11 @@
+<?php
+
+include_once $_SERVER["DOCUMENT_ROOT"]."/proaula/Models/entities/Turno.php";
+
+interface ITurnoRepository{
+    public function SaveTurno($turno):void;
+    public function UpdateTurno($turno): void;
+    public function FindTurnoById(String $turno_id): Turno;
+    public function DeleteTurno(String $turno_id):void;
+    public function FindAll(): array;
+}
