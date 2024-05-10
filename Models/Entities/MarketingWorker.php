2ab845026a6adc9ed718e6ad2da119b22945aaaa
LuisRicardo77 <luissayas2018@gmail.com>
2024-05-10 16:31:21 -0500

    agregacion de modelos de la base de datos utilizando snake_case para la tablas y para los modelos PascalCase

---
+++ Models/Entities/MarketingWorker.php
@@ -0,0 +1,7 @@
+<?php
+
+include_once $_SERVER["DOCUMENT_ROOT"]."/proaula/libs/bdConfig.php";
+
+class MarketingWorker extends ActiveRecord\Model{
+    
+}
--- Web/index.php
+++ Web/index.php
@@ -1,5 +1,7 @@
 <?php
+
 require_once $_SERVER["DOCUMENT_ROOT"]."/proaula/Controllers/vistasController.php";
+
 
 $plantilla = new vistasController();
 $plantilla->obtener_plantilla_controller();
