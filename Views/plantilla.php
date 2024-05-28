d2f4f6c34b53d00f1e6b216982a61d0776693484
LuisRicado77 <luissayas2018@gmail.com>
2024-05-27 23:21:25 -0500

    areglos en el diseño de la clase factura

--- Views/plantilla.php
+++ Views/plantilla.php
@@ -1,19 +1,7 @@
 
     <!-- Contenido dinámico -->
     <?php
-    $css_files = [
-        'principal' => 'stylesprincipal.css',
-        'nosotros' => 'stylesnosotros.css',
-        'servicios' => 'stylesservicios.css',
-        'productos' => 'stylesproductos.css',
-        'contactos' => 'stylescontactos.css',
-        'login' => 'styleslogin.css'
-    ];
-
-    $css = isset($css_files[$vista]) ? $css_files[$vista] : 'stylesprincipal.css';
-
-    $vista;
-
+    
     //echo $vista."<br>";
 
     //echo $_SERVER["DOCUMENT_ROOT"]."/proaula/Views/content/404.php";
